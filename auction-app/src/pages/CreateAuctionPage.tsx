926f04d93d5dcaaea5fd06cb4ab8cc0af736df22
Dillon Pike <DillonPike.NZ@gmail.com>
2022-06-01 23:18:15 +1200

    Added category selector and image uploader to create auction page

--- auction-app/src/pages/CreateAuctionPage.tsx
+++ auction-app/src/pages/CreateAuctionPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavigationBar from "../components/NavigationBar";
-import {Box, FilledInput, Grid, InputAdornment, InputLabel, OutlinedInput, TextField} from "@mui/material";
+import {Box, FilledInput, Grid, InputAdornment, InputLabel, OutlinedInput, Select, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
@@ -9,23 +9,32 @@ import FormControl from "@mui/material/FormControl";
 import {register, login, getUser, isLoggedIn} from "../api/api"
 import {useNavigate} from "react-router-dom";
 import {useUserStore} from "../store";
+import {DatePicker} from "@mui/x-date-pickers";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import axios from "axios";
+import AuctionListObject from "../components/AuctionListObject";
+import MenuItem from "@mui/material/MenuItem";
+import {SelectChangeEvent} from "@mui/material/Select";
 
 interface State {
     title: string,
-    category: string,
-    endDate: string,
-    image: string,
+    categoryId: string,
+    endDate: Date | null,
+    image: any,
     description: string,
     reserve: number
 }
 
 const CreateAuctionPage = () => {
 
+    const [allCategories, setAllCategories] = React.useState<Array<Category>>([]);
+    const [endDate, setEndDate] = React.useState<Date | null>(null);
     const [values, setValues] = React.useState<State>({
         title: '',
-        category: '',
-        endDate: '',
-        image: '',
+        categoryId: '1',
+        endDate: null,
+        image: null,
         description: '',
         reserve: 0
     });
@@ -43,11 +52,39 @@ const CreateAuctionPage = () => {
             })
     }, [user])
 
+    const getAllCategories = () => {
+        axios.get('http://localhost:4941/api/v1/auctions/categories')
+            .then((response) => {
+                setAllCategories(response.data)
+            })
+    }
+
+    React.useEffect(() => {
+        getAllCategories()
+    }, [setAllCategories])
+
+    const categories = () => allCategories.map((category: Category) =>
+        <MenuItem value={category.categoryId}>{category.name}</MenuItem>)
+
+    const handleCategory = (event: SelectChangeEvent) => {
+        console.log(event.target.value)
+        setValues({ ...values, categoryId: event.target.value })
+    }
+
 
     const handleChange =
         (value: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
             setValues({ ...values, [value]: event.target.value });
         };
+
+    const handleCreate = () => {
+        axios.post('http://localhost:4941/api/v1/auctions', { title: values.title, description: values.description, categoryId: values.categoryId, endDate: values.endDate })
+    }
+
+    const handleImage = (event: any) => {
+        console.log(event.target.value)
+        setValues({ ...values, endDate: event.target.files[0] });
+    }
 
     return (
         <Box>
@@ -58,19 +95,42 @@ const CreateAuctionPage = () => {
                     <TextField label="Title" variant="outlined" onChange={handleChange('title')} />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField label="Category" variant="outlined" onChange={handleChange('category')} />
+                    <FormControl sx={{minWidth: '25ch' }}>
+                        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={values.categoryId}
+                            label="Category"
+                            onChange={handleCategory}
+                        >
+                            {categories()}
+                        </Select>
+                    </FormControl>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField label="endDate" variant="outlined" onChange={handleChange('endDate')} />
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label="End Date"
+                            value={endDate}
+                            onChange={(newValue) => {
+                                setEndDate(newValue);
+                            }}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField label="Image" variant="outlined" onChange={handleChange('image')} />
+                    <input type="file" onChange={handleImage}/>
                 </Grid>
                 <Grid item xs={12}>
                     <TextField label="Description" variant="outlined" onChange={handleChange('description')} />
                 </Grid>
                 <Grid item xs={12}>
                     <TextField label="Reserve" variant="outlined" onChange={handleChange('reserve')} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" onClick={handleCreate}>Create Auction</Button>
                 </Grid>
             </Grid>
         </Box>
