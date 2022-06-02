02691c7c9970dc15a25bc114d5572437ebee9547
Dillon Pike <dkp33@uclive.ac.nz>
2022-06-02 13:40:09 +1200

    Added api calls for creating auctions and image previews

--- auction-app/src/pages/CreateAuctionPage.tsx
+++ auction-app/src/pages/CreateAuctionPage.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import NavigationBar from "../components/NavigationBar";
-import {Box, FilledInput, Grid, InputAdornment, InputLabel, OutlinedInput, Select, TextField} from "@mui/material";
+import {
+    Alert, AlertColor,
+    Box,
+    FilledInput,
+    Grid,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Select,
+    Snackbar,
+    TextField
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
@@ -9,18 +20,19 @@ import FormControl from "@mui/material/FormControl";
 import {register, login, getUser, isLoggedIn} from "../api/api"
 import {useNavigate} from "react-router-dom";
 import {useUserStore} from "../store";
-import {DatePicker} from "@mui/x-date-pickers";
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 import axios from "axios";
 import AuctionListObject from "../components/AuctionListObject";
 import MenuItem from "@mui/material/MenuItem";
 import {SelectChangeEvent} from "@mui/material/Select";
+import Cookies from "js-cookie";
 
 interface State {
     title: string,
     categoryId: string,
-    endDate: Date | null,
+    endDate: string,
     image: any,
     description: string,
     reserve: number
@@ -28,15 +40,19 @@ interface State {
 
 const CreateAuctionPage = () => {
 
+    const [snackOpen, setSnackOpen] = React.useState(false)
+    const [snackMessage, setSnackMessage] = React.useState("")
+    const [snackSeverity, setSnackSeverity] = React.useState<AlertColor>("error")
     const [allCategories, setAllCategories] = React.useState<Array<Category>>([]);
     const [endDate, setEndDate] = React.useState<Date | null>(null);
+    const [imageUrl, setImageUrl] = React.useState<string>('');
     const [values, setValues] = React.useState<State>({
         title: '',
         categoryId: '1',
-        endDate: null,
+        endDate: '',
         image: null,
         description: '',
-        reserve: 0
+        reserve: 1
     });
 
     const navigate = useNavigate();
@@ -67,10 +83,15 @@ const CreateAuctionPage = () => {
         <MenuItem value={category.categoryId}>{category.name}</MenuItem>)
 
     const handleCategory = (event: SelectChangeEvent) => {
-        console.log(event.target.value)
         setValues({ ...values, categoryId: event.target.value })
     }
 
+    const handleEndDate = (date: Date | null) => {
+        setEndDate(date);
+        if (date !== null) {
+            setValues({...values, endDate: date.toISOString().replace('Z', '').replace('T', ' ')})
+        }
+    }
 
     const handleChange =
         (value: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -78,61 +99,122 @@ const CreateAuctionPage = () => {
         };
 
     const handleCreate = () => {
-        axios.post('http://localhost:4941/api/v1/auctions', { title: values.title, description: values.description, categoryId: values.categoryId, endDate: values.endDate })
+        axios.post('http://localhost:4941/api/v1/auctions',
+            { title: values.title, description: values.description, categoryId: parseInt(values.categoryId, 10),
+                endDate: values.endDate}, { headers: { 'X-Authorization': `${Cookies.get('token')}` }})
+            .then((response) => {
+                axios.put(`http://localhost:4941/api/v1/auctions/${response.data.auctionId}/image`,
+                    values.image, { headers: { 'X-Authorization': `${Cookies.get('token')}`, 'Content-Type': values.image.type }})
+                    .then((response2) => {
+                        navigate(`/auction/${response.data.auctionId}`)
+                    }, (error) => {
+                        navigate(`/auction/${response.data.auctionId}`)
+                        setSnackMessage(`Failed to upload image`)
+                        setSnackOpen(true)
+                        setSnackSeverity("error")
+                    })
+            }, (error) => {
+                console.log(error.response)
+                setSnackMessage(`Failed to create auction`)
+                setSnackOpen(true)
+                setSnackSeverity("error")
+            })
     }
 
     const handleImage = (event: any) => {
-        console.log(event.target.value)
-        setValues({ ...values, endDate: event.target.files[0] });
+        setImageUrl(URL.createObjectURL(event.target.files[0]));
+        setValues({ ...values, image: event.target.files[0] });
+    }
+
+    const handleSnackClose = (event?: React.SyntheticEvent | Event,
+                              reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSnackOpen(false);
+    };
+
+    const snackbar = () => {
+        return (
+            <Snackbar
+                autoHideDuration={6000}
+                open={snackOpen}
+                onClose={handleSnackClose}
+                key={snackMessage}
+            >
+                <Alert onClose={handleSnackClose} severity={snackSeverity} sx={{
+                    width: '100%' }}>
+                    {snackMessage}
+                </Alert>
+            </Snackbar>
+        )
     }
 
     return (
         <Box>
             <NavigationBar/>
             <br/>
-            <Grid container spacing={1}>
-                <Grid item xs={12}>
-                    <TextField label="Title" variant="outlined" onChange={handleChange('title')} />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControl sx={{minWidth: '25ch' }}>
-                        <InputLabel id="demo-simple-select-label">Category</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={values.categoryId}
-                            label="Category"
-                            onChange={handleCategory}
-                        >
-                            {categories()}
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="End Date"
-                            value={endDate}
-                            onChange={(newValue) => {
-                                setEndDate(newValue);
-                            }}
-                            renderInput={(params) => <TextField {...params} />}
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <Grid item xs={12}>
+                        <img
+                            src={imageUrl}
+                            onError={(event: any) => {event.target.src = `https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg`}}
+                            style={{ maxHeight: 1000, maxWidth: 1000 }}
                         />
-                    </LocalizationProvider>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <label htmlFor="imageInput">
+                            <input id="imageInput" multiple accept="image/jpg, image/png, image/gif" type="file" style={{ display: "none"}} onChange={handleImage}/>
+                            <Button component="span" variant="contained">Upload Image</Button>
+                        </label>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <input type="file" onChange={handleImage}/>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Description" variant="outlined" onChange={handleChange('description')} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label="Reserve" variant="outlined" onChange={handleChange('reserve')} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained" onClick={handleCreate}>Create Auction</Button>
+                <Grid item xs={6}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                            <TextField label="Title" variant="outlined" onChange={handleChange('title')} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl sx={{minWidth: '25ch' }}>
+                                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={values.categoryId}
+                                    label="Category"
+                                    onChange={handleCategory}
+                                >
+                                    {categories()}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <DateTimePicker
+                                    label="End Date"
+                                    value={endDate}
+                                    onChange={(newValue) => {
+                                        handleEndDate(newValue);
+
+                                    }}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Description" variant="outlined" onChange={handleChange('description')} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label="Reserve" variant="outlined" onChange={handleChange('reserve')} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button variant="contained" onClick={handleCreate}>Create Auction</Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
+            {snackbar()}
         </Box>
     )
 }
